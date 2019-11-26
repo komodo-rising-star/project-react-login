863257721d9187acbc54dbc3edb49baf81f3be24
lighteagle <sofyan.agus18@gmail.com>
2019-11-26 14:23:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import "./App.css";
 
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
-<<<<<<< HEAD
 import Registration from "./components/Registration";
-=======
-<<<<<<< HEAD
-import Register from "./components/Registration"
 
-function App() {
-  return (
-    <div>
-      <Register/>
-      <Login />
-    </div>
-=======
-// import Registration from "./components/Registration";
->>>>>>> 6c66ad708ee6807ef766f06e61bad8a1f22afcd2
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import AfterLog from "./components/BannerAfterLog";
@@ -44,7 +31,6 @@ function App() {
         </PrivateRoute>
       </Switch>
     </BrowserRouter>
->>>>>>> 5b9750f959ff14b7ca3caed7c1cb24a6d7455a1e
   );
 }
 
